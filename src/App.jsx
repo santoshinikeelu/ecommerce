48ff4596c49145c1f5825238f621eefc554309d9
Santoshini <santoshini.wagonslearning@gmail.com>
2024-04-10 18:30:04 +0530

    mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -109,7 +109,7 @@ function App() {
           <img className="h-[100%] w-[80%] " src="/model-3.jpg" alt="Image" />
           <img className="h-[100%] w-[80%] sm:h-[80%] md:h-[80%] lg:h-[80%] xl:h-[80%] " src="/model-4.jpg" alt="Image" />
         </div>
-        <div className="relative flex flex-col justify-between items-center lg:grid grid-cols-3 lg:place-items-start gap-5  h-[160vh] lg:h-[40vh] w-[99vw]  mt-[73rem] lg:mt-[5rem] px-3 border-2 border-teal-300">
+        <div className="relative flex flex-col justify-between items-center lg:grid grid-cols-3 lg:place-items-start  h-[142vh] lg:h-[40vh] w-[99vw]  mt-[73rem] lg:mt-[5rem] px-3 ">
           <div className="flex flex-col justify-between items-center lg:col-span-1 lg:ml-[4rem]">
             <div className="flex flex-col justify-center items-center lg:flex lg:items-start gap-3">
               <img className="h-[55%] lg:h-[15%]  " src="/01-free-img.png" alt="Image" />
@@ -122,44 +122,44 @@ function App() {
               temporibus
             </div>
           </div>
-          <div className="flex flex-col col-span-1 mx-[2rem]">
-            <div className="flex flex-col items-center lg:flex lg:items-start gap-3">
+          <div className="flex flex-col justify-between items-center mr-[2rem] lg:col-span-1 lg:ml-[4rem]">
+            <div className="flex flex-col justify-center items-center lg:flex lg:items-start gap-3">
               <img className="h-[55%] lg:h-[15%]  " src="/02.png" alt="Image" />
               <div className=" text-black font-semibold text-[2rem] lg:text-[1.75rem]">
                 Closet Cleanse
               </div>
             </div>
-            <div className="text-[1rem] mt-5 ml-[3.5rem] font-thin ">
+            <div className="text-[1rem] lg:text-[1rem] mt-5 px-[2.5rem] lg:ml-[3.5rem] font-thin">
               Sem curae incidunt in. Illum, culpa debitis omnis convallis fugiat
               temporibus
             </div>
           </div>
-          <div className="flex flex-col col-span-1 mr-[4rem]">
-            <div className="flex flex-col items-center lg:flex lg:items-start gap-3">
+          <div className="flex flex-col justify-between items-center lg:col-span-1 lg:ml-[4rem]">
+            <div className="flex flex-col justify-center items-center mr-[2rem] lg:flex lg:items-start gap-3">
               <img className="h-[55%] lg:h-[15%] " src="/03.png" alt="Image" />
               <div className=" text-black font-semibold text-[2rem] lg:text-[1.75rem]">
                 Shopping Tour
               </div>
             </div>
-            <div className="text-[1rem] mt-5 ml-[3.5rem] font-thin ">
+            <div className="text-[1rem] lg:text-[1rem] mt-5 px-[2.5rem] lg:ml-[3.5rem] font-thin ">
               Sem curae incidunt in. Illum, culpa debitis omnis convallis fugiat
               temporibus
             </div>
           </div>
         </div>
-        <div className="h-[85vh]  bg-red-50 flex flex-col items-center ">
-          <div className="text-black text-[1.5rem] mt-[6rem] underline underline-offset-[1.75rem] decoration-[0.15rem] px-4">
+        <div className="h-[119vh]  bg-red-50 flex flex-col items-center mt-[1rem] ">
+          <div className="text-black text-[1rem] lg:text-[1.5rem] mt-[6rem] underline underline-offset-[1.05rem] lg:underline-offset-[1.75rem] decoration-[0.1rem] lg:decoration-[0.15rem] px-4">
             Testimonials
           </div>
-          <div className="text-black text-[3rem] mt-[2rem]  px-4">
+          <div className="text-black text-[2rem] lg:text-[3rem] mt-[2rem]  px-4">
             My Happy Clients!
           </div>
           <div className="text-black text-[0.95rem] mt-[2rem] flex flex-col">
-            <span className="mx-[10rem]">
+            <span className=" px-[4rem] lg:mx-[10rem] ">
               Aliquip quae scipit eros. Anim commodi maiores pharetra, arcu
               incidunt, omnis iste
             </span>{" "}
-            <span className="px-[11rem]">
+            <span className="px-[4rem] lg:px-[11rem] ">
               incidunt cursus. Officia ea numquam vivamus repellen. Minus
               fermentum litora.
             </span>
@@ -175,15 +175,15 @@ function App() {
             <span className="ml-[]">Deborah Sims</span>
           </div>
         </div>
-        <div className="text-black text-[0.95rem]  grid grid-cols-6 place-items-center gap-5  mt-[5rem] px-8">
+        <div className="text-black text-[0.95rem]  grid grid-cols-2 lg:grid-cols-6 place-items-center gap-7 lg:gap-5  mt-[5rem] px-8">
           <img
-            className="h-[2.95rem] w-[10rem] mr-2 "
+            className=" h-[2.95rem] w-[10rem] mr-2 "
             loading="lazy"
             src="/logo-5.png"
             alt="Image"
           />
           <img
-            className="h-[2.95rem] w-[10rem] mr-2 "
+            className=" h-[2.95rem] w-[10rem] mr-2 "
             loading="lazy"
             src="/logo.png"
             alt="Image"
@@ -213,9 +213,9 @@ function App() {
             alt="Image"
           />
         </div>
-        <div className="h-[85vh] relative parallax-container flex flex-col  place-items-start mt-[7rem] gap-4">
-        <div className="text-black text-[0.95rem]  grid grid-cols-3 place-items-center gap-5  mt-[5rem] px-8 h-[88vh] ">
-          <div className="  bg-[#C2A09E] col-span-1 h-full w-full place-items-start">
+        <div className="">
+        <div className="text-black text-[0.95rem]  lg:grid lg:grid-cols-3 place-items-center gap-5 mt-3 lg:mt-[5rem] px-5 lg:px-8 h-[98vh] ">
+          <div className="  bg-[#C2A09E] lg:col-span-1 h-full w-full place-items-start">
             <div className="text-white/85 text-[3rem] mt-[3rem] mx-[2rem]">
               Area of Expertise!
             </div>
@@ -241,7 +241,7 @@ function App() {
                 <IoMdArrowDropright size={18} />{" "}
               </span>
             </div>
-            <div className="text-black text-[1rem] mt-[2rem] mx-[2rem] font-bold  flex justify-between items-start border-b-[0.05rem] border-white/40 py-[0.5rem]">
+            <div className="text-black text-[1rem] mt-[2rem] mx-[2rem] font-bold  flex justify-between items-start  py-[0.5rem]">
               <span>Before-After</span>
               <span>
                 {" "}
@@ -249,21 +249,21 @@ function App() {
               </span>
             </div>
           </div>
-          <div className=" col-span-1 h-full w-full flex flex-col place-items-start">
+          <div className=" col-span-1 h-[130vh] w-full flex flex-col  place-items-start">
             <img
-              className="h-[15rem] w-full mr-2 p-4 "
+              className="lg:h-[15rem] w-full mr-2 px-[0.05rem] pt-[4rem] lg:p-4 "
               loading="lazy"
               src="/fashion-shopping.jpg"
               alt="Image"
             />
-            <div className="text-[1.5rem] px-5">
+            <div className="text-[1.5rem] px-1 pt-2 lg:px-5">
               I've Worked with from Big Agencies to New Bloggers.
             </div>
-            <div className="text-[0.95rem] px-5 mt-[2rem]  ">
+            <div className="text-[0.95rem] px-2 lg:px-5 mt-4 lg:mt-[2rem]  ">
               Molestiae purus facilisi nonummy soluta nisi odit, ac taciti
               voluptatibus, eveniet veritatis cubilia magna. Imperdiet.
             </div>
-            <div className="mt-[2rem] px-5">
+            <div className="mt-[2rem] px-2 lg:px-5">
               <button
                 class="  border-[0.05rem] border-red-300 text-red-300
          text-[0.95rem]  px-[2rem] py-[0.5rem] transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-50 "
@@ -278,21 +278,21 @@ function App() {
               </button>
             </div>
           </div>
-          <div className=" col-span-1 h-full w-full flex flex-col place-items-start">
+          <div className=" col-span-1 h-[130vh] w-full flex flex-col place-items-start">
             <img
-              className="h-[15rem] w-full mr-2 p-4 "
+              className="lg:h-[15rem] w-full mr-2 px-[0.05rem] lg:p-4"
               loading="lazy"
               src="/makeup-kit.jpg"
               alt="Image"
             />
-            <div className="text-[1.5rem] px-5">
+            <div className="text-[1.5rem] px-1 pt-2 lg:px-5">
               I Can Help You Take Your next Step Regardless of Size.
             </div>
-            <div className="text-[0.95rem] px-5 mt-[2rem]">
+            <div className="text-[0.95rem] px-2 lg:px-5 mt-4 lg:mt-[2rem] ">
               Molestiae purus facilisi nonummy soluta nisi odit, ac taciti
               voluptatibus, eveniet veritatis cubilia magna. Imperdiet.
             </div>
-            <div className="mt-[2rem] px-5">
+            <div className="mt-[2rem] px-2 lg:px-5">
               <button
                 class="  border-[0.05rem] border-red-300 text-red-300
          text-[0.95rem]  px-[2rem] py-[0.5rem] transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-50 "
@@ -308,22 +308,24 @@ function App() {
             </div>
           </div>
         </div>
-          <div className="text-white/85 text-[1.5rem] underline underline-offset-[1.75rem] decoration-[0.15rem] px-4 top-2">
+        </div>
+         <div className="h-[165vh] relative parallax-container flex flex-col  place-items-start mt-[67rem] lg:mt-[20rem] gap-">
+         <div className="text-white/85 text-[1rem] lg:text-[1.5rem] underline underline-offset-[1.75rem] decoration-[0.15rem] px-4 mt-[0.25rem] lg:top-2">
             Contact Me
           </div>
-          <div className="text-white/85 text-[3rem] px-4 ">
+          <div className="text-white/85 text-[1.7rem] lg:text-[3rem] px-4 ">
             Consultations Are Free!
           </div>
           <div className="text-white/85 text-[0.95rem] ">
-            <span className="mx-[10rem]">
+            <span className=" lg:mx-[10rem]">
               Platea proident! Aute, rerum sociosqu numquam placerat, rerum
               molestias doloribus incidunt occaecati placeat,
             </span>{" "}
-            <span className="px-[11rem]">
+            <span className="lg:px-[11rem]">
               repellendus mus optio, totam cons.
             </span>
           </div>
-          <div className=" px-[5rem]">
+          <div className="px-2 lg:px-[5rem]">
             <button
               class="border-[0.05rem] text-white/85 
          text-[0.92rem]    px-[4rem] py-[0.5rem] transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-50 "
@@ -337,7 +339,7 @@ function App() {
               </div>
             </button>
           </div>
-        </div>
+         </div>
     </div>
   );
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,18 +2,16 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex px-4 bg-black">
-      <div className="grid grid-cols-2 sm:flex sm:flex-col w-full p-8 ">
-        <div className=" flex  text-white  col-span-1  place-items-start p-1 text-sm md:text-lg lg:text-xl xl:text-2xl ">
-        Copyright © 2024 Fashion Lifestyle Blog
+    <div className="flex  px-6 lg:px-4 bg-black">
+      <div className=" flex flex-col lg:grid lg:grid-cols-2 sm:flex sm:flex-col w-full p-3 lg:p-8 ">
+        <div className=" flex  text-white  col-span-1 place-items-center lg:place-items-start mx-[1remrem]  p-1 text-sm md:text-lg lg:text-xl xl:text-2xl ">
+          Copyright © 2024 Fashion Lifestyle Blog
         </div>
-        <div className="flex gap-7 place-content-end items-center col-span-1 p-3  font-semibold text-sm text-white">
+        <div className="flex gap-7 place-items-center lg:place-content-end items-center col-span-1 px-12 p-2 lg:p-3  font-semibold text-sm text-white">
           <span className="cursor-pointer ">Home</span>
           <span className="cursor-pointer ">Blog</span>
-          <span className="cursor-pointer ">New Arrivals</span>
           <span className="cursor-pointer ">Contact</span>
         </div>
-       
       </div>
     </div>
   );
